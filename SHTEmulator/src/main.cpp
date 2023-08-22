1323c183b458f9f451713736802b2411bd43b5df
Kyle Benne <kyle.benne@nrel.gov>
2023-08-22 10:43:02 -0500

    Add emulator for SHT sensor

--- SHTEmulator/src/main.cpp
+++ SHTEmulator/src/main.cpp
@@ -4,50 +4,48 @@
 #include <WebServer.h>
 #include "registers.hpp"
 
-//byte Address;
-//WebServer server(80);
-//constexpr char ssid[] = EMBEDDED_SSID; //  your network SSID
-//constexpr char pass[] = EMBEDDED_PASS; //  your network password
+byte Address;
+WebServer server(80);
+constexpr char ssid[] = EMBEDDED_SSID; //  your network SSID
+constexpr char pass[] = EMBEDDED_PASS; //  your network password
 
 
 // Set the raw sensor reading (adc_T) given temperature T in degC
 // This solves the quadratic equation given in Appendix 8.1 in the BME datasheet
 void set_T(const double &T) {
-  TemperatureRegister = (T + 45) * (pow(2, 16) - 1) / 175;
+  // This is an ecobee specific adjustment that should get the display to
+  // nearly match the value provided by the sensor
+  double T_adjusted = (T + 4.3766) / 0.9861;
+  // This is the function provided by the datasheet
+  TemperatureRegister = (T_adjusted + 45) * (pow(2, 16) - 1) / 175;
+}
+
+double get_T(void) {
+  double T_adjusted = TemperatureRegister * 175 / (pow(2, 16) - 1) - 45;
+  return T_adjusted * 0.9861 - 4.3766;
 }
 
 void set_H(const double &H) {
-  HumidityRegister = H * (pow(2, 16) - 1) / 100;
+  double H_adjusted = H; // TODO need a humidity correction curve as function of H AND T
+  HumidityRegister = (H_adjusted + 6) * (pow(2, 16) - 1) / 125;
 }
 
 int16_t Command;
 
+//#define SHT4x_READSERIAL 0x89 /**< Read Out of Serial Register */
+//#define SHT4x_SOFTRESET 0x94  /**< Soft Reset */
+
 bool is_measure_command(int16_t command) {
-  return (command == SHT31_MEAS_HIGHREP_STRETCH) ||
-         (command == SHT31_MEAS_MEDREP_STRETCH)  ||
-         (command == SHT31_MEAS_LOWREP_STRETCH)  ||
-         (command == SHT31_MEAS_HIGHREP)         ||
-         (command == SHT31_MEAS_MEDREP)          ||
-         (command == SHT31_MEAS_LOWREP);
-}
+  return (command == SHT4x_NOHEAT_HIGHPRECISION) ||
+         (command == SHT4x_NOHEAT_MEDPRECISION) ||
+         (command == SHT4x_NOHEAT_LOWPRECISION) ||
+         (command == SHT4x_HIGHHEAT_1S) ||
+         (command == SHT4x_HIGHHEAT_100MS) ||
+         (command == SHT4x_MEDHEAT_1S) ||
+         (command == SHT4x_MEDHEAT_100MS) ||
+         (command == SHT4x_LOWHEAT_1S) ||
+         (command == SHT4x_LOWHEAT_100MS);
 
-void clear_status() {
-  // 0 is normal (all systems go) status
-  StatusRegister = 0x0;
-}
-
-void reset_status() {
-  // See datasheet for status register bit assignments
-  // On startup the "reset" bit and the "alert pending" bits are set
-  StatusRegister = 0x8008;
-}
-
-void enable_heater() {
-  StatusRegister = StatusRegister | SHT31_REG_HEATER_MASK;
-}
-
-void disable_heater() {
-  StatusRegister = StatusRegister & ~SHT31_REG_HEATER_MASK;
 }
 
 uint8_t crc8(const uint8_t *data, int len) {
@@ -78,41 +76,34 @@ uint8_t crc8(const uint8_t *data, int len) {
 void eval_command(int16_t command) {
   // Some commands setup an expected read request, which require no action. (until the read request)
   // Other commands are a command to write data, and they should be handled here.
-  if (command == SHT31_CLEARSTATUS) {
-    clear_status();
-  } else if (command == SHT31_SOFTRESET) {
-    reset_status();
-  } else if (command == SHT31_HEATEREN) {
-    enable_heater(); 
-  } else if (command == SHT31_HEATERDIS) {
-    disable_heater();
+  // Update: Actually for SHT4x (unlike SHT3x), there are no write commands, beyond reset
+  if (command == SHT4x_SOFTRESET) {
+    Serial.println("SOFTRESET Issued");
   }
 }
 
 void on_wire_receive(int num_bytes) {
   Serial.println("Begin on_wire_receive");
 
-  if (num_bytes == 2) {
-    byte data[2];
+  if (num_bytes == 1) {
     int index = 0;
 
     while (Wire.available()) {
-      if (index > 1) {
+      if (index > 0) {
         Serial.println("Unexpected data received from controller");
         break;
       }
-      data[index] = Wire.read();
+      Command = Wire.read();
       index++; 
     }
 
     // Second byte in the command is the last significant digit
-    Command = int16_t(data[0] << 8) | int16_t(data[1]);
     eval_command(Command);
 
     Serial.print("Received command ");
     Serial.println(Command, HEX);
   } else {
-    Serial.print("Two bytes were expected, but received ");
+    Serial.print("One byte was expected, but received ");
     Serial.print(num_bytes, 0);
     Serial.println(" bytes from controller");
     Command = 0x0;
@@ -126,13 +117,7 @@ void on_wire_request(void) {
   Serial.print("Current Command: ");
   Serial.println(Command, HEX);
 
-  if (Command == SHT31_READSTATUS) {
-    byte data[3];
-    data[0] = StatusRegister >> 8;
-    data[1] = StatusRegister;
-    data[2] = crc8(data, 2);
-    Wire.write(data, 3);
-  } else if (is_measure_command(Command)) {
+  if (is_measure_command(Command)) {
     byte data[6];
     data[0] = TemperatureRegister >> 8;
     data[1] = TemperatureRegister;
@@ -141,65 +126,97 @@ void on_wire_request(void) {
     data[4] = HumidityRegister;
     data[5] = crc8(data + 3, 2);
     Wire.write(data, 6);
+  } else if(Command == SHT4x_READSERIAL) {
+    Wire.write(SerialNumber, 6);
   }
 
   Serial.println("End on_wire_request");
 }
 
-//void http_api_endpoint() {
-//  const auto method = server.method();
-//  if (method == HTTP_GET) {
-//    // Need to either cash the current T, or implement the calibration function
-//    // to get it back out of the adc_T.
-//    server.send(200, "text/plain", "Not implemented"); 
-//  } else if (method == HTTP_PUT) {
-//    if (server.hasArg("temperature")) {
-//      const auto value = server.arg("temperature");
-//      const double new_temp = value.toDouble();
-//      set_T(new_temp);
-//      server.send(200); 
-//    }
-//    server.send(400, "text/plain", "Bad request");
-//  } else {
-//    server.send(405, "text/plain", "Method not allowed");
-//  }
-//}
-//
-//void http_not_found_endpoint(){
-//  server.send(404, "text/plain", "Not found");
-//}
+// TODO: Reduce code duplication in endpoints
+void http_temperature_endpoint() {
+  const auto method = server.method();
+  if (method == HTTP_GET) {
+    // Need to either cash the current T, or implement the calibration function
+    // to get it back out of the adc_T.
+    server.send(200, "text/plain", "Not implemented"); 
+  } else if (method == HTTP_PUT) {
+    if (server.hasArg("value")) {
+      const auto value = server.arg("value");
+      const double new_temp = value.toDouble();
+      set_T(new_temp);
+      server.send(200); 
+    }
+    server.send(400, "text/plain", "Bad request");
+  } else {
+    server.send(405, "text/plain", "Method not allowed");
+  }
+}
+
+void http_humidity_endpoint() {
+  const auto method = server.method();
+  if (method == HTTP_GET) {
+    // Need to either cash the current T, or implement the calibration function
+    // to get it back out of the adc_T.
+    server.send(200, "text/plain", "Not implemented"); 
+  } else if (method == HTTP_PUT) {
+    if (server.hasArg("value")) {
+      const auto value = server.arg("value");
+      const double new_h = value.toDouble();
+      set_H(new_h);
+      server.send(200); 
+    }
+    server.send(400, "text/plain", "Bad request");
+  } else {
+    server.send(405, "text/plain", "Method not allowed");
+  }
+}
+
+void http_not_found_endpoint(){
+  server.send(404, "text/plain", "Not found");
+}
+
+void init_serial_number() {
+  SerialNumber[0] = 0x0E;
+  SerialNumber[1] = 0xFE;
+  SerialNumber[2] = crc8(SerialNumber, 2);
+  SerialNumber[3] = 0x7F;
+  SerialNumber[4] = 0xBF;
+  SerialNumber[5] = crc8(SerialNumber + 3, 2);
+}
 
 void setup() {
   Serial.begin(9600);
 
+  init_serial_number();
   // Set T in degress C and the emulator will communicate this value
   // over I2C as if connected to a normal BME280
-  reset_status();
-  set_T(28.13);
+  set_T(22.00);
   set_H(50.0);
 
   Wire.onReceive(on_wire_receive);
   Wire.onRequest(on_wire_request);
-  Wire.begin(SHT31_DEFAULT_ADDR); // This is the I2C address of a BME280
+  Wire.begin(SHT4x_DEFAULT_ADDR); // This is the I2C address of a BME280
   Serial.println("I2C Wire protocol started");
 
-  //const auto status = WiFi.begin(ssid, pass);
-  //if ( status != WL_CONNECTED) {
-  //  Serial.println("Could not connect to wireless network");
-  //} else {
-  //  const auto ip = WiFi.localIP();
-  //  Serial.print("IP address is: ");
-  //  Serial.println(ip);
-  //}
+  const auto status = WiFi.begin(ssid, pass);
+  if ( status != WL_CONNECTED) {
+    Serial.println("Could not connect to wireless network");
+  } else {
+    const auto ip = WiFi.localIP();
+    Serial.print("IP address is: ");
+    Serial.println(ip);
+  }
 
-  //server.on("/api", http_api_endpoint);
-  //server.onNotFound(http_not_found_endpoint);
-  //server.begin();
-  //Serial.println("HTTP server started");
+  server.on("/api/temperature", http_temperature_endpoint);
+  server.on("/api/humidity", http_humidity_endpoint);
+  server.onNotFound(http_not_found_endpoint);
+  server.begin();
+  Serial.println("HTTP server started");
 }
 
 void loop() {
-  delay(2000);
+  //delay(20);
 
-  //server.handleClient();
+  server.handleClient();
 }
--- SHTEmulator/src/registers.hpp
+++ SHTEmulator/src/registers.hpp
@@ -1,23 +1,28 @@
-int16_t StatusRegister;
 int16_t TemperatureRegister;
 int16_t HumidityRegister;
 
-#define SHT31_DEFAULT_ADDR 0x44 /**< SHT31 Default Address */
-#define SHT31_MEAS_HIGHREP_STRETCH                                             \
-  0x2C06 /**< Measurement High Repeatability with Clock Stretch Enabled */
-#define SHT31_MEAS_MEDREP_STRETCH                                              \
-  0x2C0D /**< Measurement Medium Repeatability with Clock Stretch Enabled */
-#define SHT31_MEAS_LOWREP_STRETCH                                              \
-  0x2C10 /**< Measurement Low Repeatability with Clock Stretch Enabled*/
-#define SHT31_MEAS_HIGHREP                                                     \
-  0x2400 /**< Measurement High Repeatability with Clock Stretch Disabled */
-#define SHT31_MEAS_MEDREP                                                      \
-  0x240B /**< Measurement Medium Repeatability with Clock Stretch Disabled */
-#define SHT31_MEAS_LOWREP                                                      \
-  0x2416 /**< Measurement Low Repeatability with Clock Stretch Disabled */
-#define SHT31_READSTATUS 0xF32D   /**< Read Out of Status Register */
-#define SHT31_CLEARSTATUS 0x3041  /**< Clear Status */
-#define SHT31_SOFTRESET 0x30A2    /**< Soft Reset */
-#define SHT31_HEATEREN 0x306D     /**< Heater Enable */
-#define SHT31_HEATERDIS 0x3066    /**< Heater Disable */
-#define SHT31_REG_HEATER_MASK 0x2000 /**< Status Register Heater Mask */
+#define SHT4x_DEFAULT_ADDR 0x44 /**< SHT4x I2C Address */
+#define SHT4x_NOHEAT_HIGHPRECISION                                             \
+  0xFD /**< High precision measurement, no heater */
+#define SHT4x_NOHEAT_MEDPRECISION                                              \
+  0xF6 /**< Medium precision measurement, no heater */
+#define SHT4x_NOHEAT_LOWPRECISION                                              \
+  0xE0 /**< Low precision measurement, no heater */
+
+#define SHT4x_HIGHHEAT_1S                                                      \
+  0x39 /**< High precision measurement, high heat for 1 sec */
+#define SHT4x_HIGHHEAT_100MS                                                   \
+  0x32 /**< High precision measurement, high heat for 0.1 sec */
+#define SHT4x_MEDHEAT_1S                                                       \
+  0x2F /**< High precision measurement, med heat for 1 sec */
+#define SHT4x_MEDHEAT_100MS                                                    \
+  0x24 /**< High precision measurement, med heat for 0.1 sec */
+#define SHT4x_LOWHEAT_1S                                                       \
+  0x1E /**< High precision measurement, low heat for 1 sec */
+#define SHT4x_LOWHEAT_100MS                                                    \
+  0x15 /**< High precision measurement, low heat for 0.1 sec */
+
+#define SHT4x_READSERIAL 0x89 /**< Read Out of Serial Register */
+#define SHT4x_SOFTRESET 0x94  /**< Soft Reset */
+
+byte SerialNumber[6];
